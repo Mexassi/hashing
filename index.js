330232d46305b675db29a78a201b7173fcefa7c8
Mexassi <mexassi85@gmail.com>
2019-08-13 11:18:30 +1000

    adding sha algorithms

--- index.js
+++ index.js
@@ -5,19 +5,58 @@
     const md5 = require('md5');
     const inquirer = require('inquirer');
     const chalk = require('chalk');
+    const hash = require('hash.js');
+
+    const Algos = {
+        MD5: 'md5',
+        SHA1: 'sha1',
+        SHA224: 'sha224',
+        SHA256: 'sha256',
+        SHA512: 'sha512'
+    };
 
     inquirer
         .prompt([
             {
-                type: 'input',
-                name: 'text',
-                message: 'Please enter the text to hash:'
+                type: 'list',
+                name: 'algo',
+                message: 'Which hashing algorithm do you want to use?',
+                choices: [Algos.MD5, Algos.SHA1, Algos.SHA224, Algos.SHA256, Algos.SHA512]
             }
         ])
         .then(answers => {
-            const text = answers.text;
+            const algo = answers.algo;
 
-            console.log(`the text to hash is ${chalk.cyan(text)}`);
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'text',
+                        message: 'Please enter the text to hash:'
+                    }
+                ])
+                .then(answers => {
+                    const text = answers.text;
+                    
+                    let hashed = null;
+
+                    switch (algo) {
+                        case Algos.MD5:
+                            hashed = md5(text);
+                            break;
+                        case Algos.SHA1:
+                        case Algos.SHA224:
+                        case Algos.SHA256:
+                        case Algos.SHA512:
+                            hashed = hash[algo]().update(text).digest('hex');
+
+                            break;
+                        default:
+                            throw new Error(`${algo} not supported`);
+                    }
+                
+                    console.log(`your ${algo} hash is: ${chalk.cyan(hashed)}`);
+                });
         });
 
 })();
